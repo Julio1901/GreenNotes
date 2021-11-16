821881717e006fde1fb2e800efc83a94f20fc974
João Victor Mendes da Silva <mendes.silva.jv@gmail.com>
2021-11-16 12:39:10 -0300

    added a comment

--- app/src/main/java/com/julio/greennotes/MainActivity.kt
+++ app/src/main/java/com/julio/greennotes/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Os jacaré dominou o projeto kskk
+        //Green notes
     }
 }
