6d5c022b896511b7b1ab028fb77a719652b191ab
Milaniingrid <milanisingrid@gmail.com>
2021-11-16 12:53:42 -0300

    https://github.com/Julio1901/GreenNotes

--- app/src/main/java/com/julio/greennotes/MainActivity.kt
+++ app/src/main/java/com/julio/greennotes/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // Os jacaré dominou o projeto kskk
         //Green notes
+        //oi equipe bonita
     }
 }
