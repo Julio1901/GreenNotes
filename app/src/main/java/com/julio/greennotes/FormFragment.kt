e56e361abfadacad6e8bdd885c89525b3bd29b87
Julio Cesar <js.cesar42@gmail.com>
2021-11-29 19:49:44 -0300

    Implemented API. App working

--- app/src/main/java/com/julio/greennotes/FormFragment.kt
+++ app/src/main/java/com/julio/greennotes/FormFragment.kt
@@ -18,7 +18,10 @@ import org.koin.core.parameter.parametersOf
 
 class FormFragment : Fragment() {
 
-    private val taskViewModel : TaskViewModel by viewModel()
+
+    private val taskViewModel : TaskViewModel by viewModel{
+        parametersOf(TaskRepository())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,47 +35,15 @@ class FormFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val button = view.findViewById<Button>(R.id.btn_save)
-        //val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view_oficial)
-        //val cardView : CardView = view.findViewById(R.id.cardView_formTask)
+
 
         button.setOnClickListener {
 
-            val fakeTask = Task(9,"TEST DATE FORMAT variavel", "Fake Description", "Fake responsible",
+            val fakeTask = Task(11,"TEST GREEN NOTES", "Fake Description", "Fake responsible",
                 "2021-11-29 10:12:00", "Fake status")
 
             taskViewModel.addTask(fakeTask)
-
-//            val name = view.plainText_task_title.text.toString()
-//            val details = view.plainText_task_details.text.toString()
-//            val responsible = view.plainText_responsible.text.toString()
-//            val date = view.plainText_date.text.toString()
-//            val status = view.plainText_progress.text.toString()
-//
-//            //val newTask = Task.createNewTaskWithView(view)
-//            val newTask = TaskOld(name, details, responsible, date, status)
-//                val taskCategoryObj = TaskCategory("test")
-//
-//                if(taskCategoryObj.addTask(newTask)){
-//                    val action = FormFragmentDirections.actionTest()
-//                    findNavController().navigate(action)
-//                }else{
-//                    var campoVazio = ""
-//                    if (name.isEmpty()) {
-//                        campoVazio = "title"
-//                    }else if (details.isEmpty()){
-//                        campoVazio = "details"
-//                    }else if (responsible.isEmpty()){
-//                        campoVazio = "responsible"
-//                    }else if (date.isEmpty()){
-//                        campoVazio = "date"
-//                    }else if (status.isEmpty()){
-//                        campoVazio = "progress"
-//                    }
-//                    Toast.makeText(this.context, "Campo $campoVazio está vazio", Toast.LENGTH_LONG).show()
-//                }
         }
     }
-
-
 
 }
