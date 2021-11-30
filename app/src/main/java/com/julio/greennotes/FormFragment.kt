da9e4e05a02378d701de210cbc8f0bb0634b0c93
Julio Cesar <js.cesar42@gmail.com>
2021-11-29 22:00:39 -0300

    date picker implemented with validation filds

--- app/src/main/java/com/julio/greennotes/FormFragment.kt
+++ app/src/main/java/com/julio/greennotes/FormFragment.kt
@@ -1,13 +1,16 @@
 package com.julio.greennotes
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.julio.greennotes.model.Task
 import com.julio.greennotes.service.TaskOld
 import com.julio.greennotes.service.TaskCategory
@@ -15,6 +18,10 @@ import kotlinx.android.synthetic.main.fragment_form.view.*
 import com.julio.greennotes.repository.TaskRepository
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.util.*
 
 class FormFragment : Fragment() {
 
@@ -35,15 +42,78 @@ class FormFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val button = view.findViewById<Button>(R.id.btn_save)
+        val plainTextTitle = view.findViewById<EditText>(R.id.plainText_task_title)
+        val plainTextDetails = view.findViewById<EditText>(R.id.plainText_task_details)
+        val plainTextResponsible = view.findViewById<EditText>(R.id.plainText_responsible)
+        val plainTextDate = view.findViewById<EditText>(R.id.plainText_date)
+        val plainTextProgress = view.findViewById<EditText>(R.id.plainText_progress)
 
+        //TODO: Refactor to open with one click
+        //Calendary
+        plainTextDate.setOnClickListener {
+            val selecionadorDeData = MaterialDatePicker
+                .Builder.datePicker().build()
+            selecionadorDeData.show(this.parentFragmentManager, "MATERIAL_DATE_PICKER")
+            selecionadorDeData
+                .addOnPositiveButtonClickListener { dataEmMilisegundos ->
+                    val data = Instant.ofEpochMilli(dataEmMilisegundos)
+                        .atZone(ZoneId.of("America/Sao_Paulo"))
+                        .withZoneSameInstant(ZoneId.ofOffset("UTC", ZoneOffset.UTC))
+                        .toLocalDate()
+                    Log.i("MaterialDatePicker", "data com LocalDate: $data")
+                    //TODO: Replace it with get current hour in system
+                    val formatedDate = (data.year.toString() + "-" +
+                            data.monthValue.toString() + "-" +
+                            data.dayOfMonth.toString() + " " +
+                            "10:" +
+                            "12:"+
+                            "00")
+
+                    plainTextDate.setText(formatedDate)
+                    Log.i("MinhaData", formatedDate)
+
+                }
+        }
 
         button.setOnClickListener {
+            val title = plainTextTitle.text.toString()
+            val details = plainTextDetails.text.toString()
+            val responsible = plainTextResponsible.text.toString()
+            val formatedData = plainTextDate.text.toString()
+            val progress = plainTextProgress.text.toString()
 
-            val fakeTask = Task(11,"TEST GREEN NOTES", "Fake Description", "Fake responsible",
-                "2021-11-29 10:12:00", "Fake status")
-
-            taskViewModel.addTask(fakeTask)
+            //Validate fields
+            if(!validateFields(title, details, responsible,formatedData,progress)){
+                //TODO: Refactor this to generate auto id
+                val newTask = Task(15,title,details,responsible,formatedData,progress)
+                taskViewModel.addTask(newTask)
+            }
         }
+    }
+
+
+    fun validateFields(title: String, details: String, responsible: String, formatedDate: String, progress : String) : Boolean {
+        var campoVazio = ""
+        var containsEmptyField = false
+
+
+        if (title.isEmpty()) {
+            campoVazio = "title"
+        }else if (details.isEmpty()){
+            campoVazio = "details"
+        }else if (responsible.isEmpty()){
+            campoVazio = "responsible"
+        }else if (formatedDate.isEmpty()){
+            campoVazio = "date"
+        }else if (progress.isEmpty()){
+            campoVazio = "progress"
+        }
+        //TODO: Refactor this
+        if (campoVazio != ""){
+            Toast.makeText(this.context, "Campo $campoVazio está vazio", Toast.LENGTH_LONG).show()
+            containsEmptyField = true
+        }
+        return containsEmptyField
     }
 
 }
